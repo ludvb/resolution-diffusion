e730d6987df2b2340a6beeee561d340e49fd9f1b
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2021-08-25 16:28:18 +0200

    add support for distributed training

--- resolution_diffusion/__main__.py
+++ resolution_diffusion/__main__.py
@@ -2,9 +2,9 @@ import argparse
 import os
 import itertools as it
 import logging
+import subprocess
 import sys
 from datetime import datetime
-from subprocess import run
 
 import numpy as np
 import torch
@@ -19,7 +19,7 @@ from tqdm import tqdm
 from .common import first_unique_filename, interpolate, interpolate_samples
 from .model import Model
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+world_size = torch.cuda.device_count() if torch.cuda.is_available() else 1
 
 
 def main():
@@ -31,6 +31,7 @@ def main():
     argparser.add_argument("--dataset", type=str, default="MNIST")
     argparser.add_argument("--batch-size", type=int, default=32)
     argparser.add_argument("--features", type=int, default=32)
+    argparser.add_argument("--seed", type=int, default=0)
     argparser.add_argument(
         "--save-path",
         type=str,
@@ -38,18 +39,41 @@ def main():
     )
     options = argparser.parse_args()
 
-    os.makedirs(os.path.join(options.save_path, "checkpoints"), exist_ok=True)
-    os.makedirs(os.path.join(options.save_path, "tb"), exist_ok=True)
-    logging.basicConfig(
-        filename=first_unique_filename(os.path.join(options.save_path, "log")),
-        level=logging.DEBUG,
-        format="[%(asctime)s]  (%(levelname)s)  %(message)s",
-        encoding="utf-8",
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+    if world_size == 1:
+        run(0, options)
+    else:
+        torch.multiprocessing.spawn(run, nprocs=world_size, args=(options,))
+
+
+def run(rank, options):
+    torch.distributed.init_process_group(
+        backend="nccl",
+        world_size=world_size,
+        rank=rank,
     )
+
+    if rank == 0:
+        os.makedirs(os.path.join(options.save_path, "checkpoints"), exist_ok=True)
+        os.makedirs(os.path.join(options.save_path, "tb"), exist_ok=True)
+        logging.basicConfig(
+            filename=first_unique_filename(os.path.join(options.save_path, "log")),
+            level=logging.DEBUG,
+            format="[%(asctime)s]  (%(levelname)s)  %(message)s",
+            encoding="utf-8",
+        )
+    else:
+        logging.basicConfig(
+            filename=first_unique_filename(os.path.join(options.save_path, "log")),
+            level=logging.WARNING,
+            format="[%(asctime)s]  $(process)d  (%(levelname)s)  %(message)s",
+            encoding="utf-8",
+        )
 
     logging.info(
         "Commit: {}".format(
-            run(
+            subprocess.run(
                 ["git", "describe", "--always", "--long", "--dirty"],
                 capture_output=True,
             )
@@ -79,10 +103,15 @@ def main():
     dataloader = DataLoader(
         dataset,
         batch_size=options.batch_size,
-        drop_last=True,
         num_workers=len(os.sched_getaffinity(0)),
         persistent_workers=True,
-        shuffle=True,
+        sampler=torch.utils.data.distributed.DistributedSampler(
+            dataset,
+            num_replicas=world_size,
+            rank=rank,
+            shuffle=True,
+            drop_last=True,
+        ),
     )
 
     incremental_scale = options.incremental_scale
@@ -90,10 +119,15 @@ def main():
     num_steps = int(np.ceil(np.log(data_dim) / np.log(incremental_scale)))
     scale_factors = [1 / incremental_scale ** k for k in range(0, num_steps)]
 
+    device = torch.device(f"cuda:{rank}" if torch.cuda.is_available() else "cpu")
+    torch.manual_seed(options.seed)
     model = Model(
         incremental_scale=incremental_scale, num_latent_features=options.features
+    ).to(device)
+    model = torch.nn.parallel.DistributedDataParallel(
+        model.to(device=device), device_ids=[device], output_device=device
     )
-    model = model.to(device=device)
+
     optim = torch.optim.Adam(model.parameters(), lr=options.learning_rate)
 
     if options.checkpoint is not None:
@@ -102,10 +136,23 @@ def main():
         model.load_state_dict(checkpoint["model"])
         optim.load_state_dict(checkpoint["optim"])
 
-    summary_writer = SummaryWriter(os.path.join(options.save_path, "tb"))
+    if rank == 0:
+        return run_training_rank0(
+            options.save_path, dataloader, model, optim, scale_factors, device
+        )
+    return run_training_rankX(dataloader, dataloader, optim, scale_factors, device)
+
+
+def run_training_rankX(dataloader, model, optim, scale_factors, device):
+    for _ in training_loop(dataloader, model, optim, scale_factors, device):
+        pass
+
+
+def run_training_rank0(save_path, dataloader, model, optim, scale_factors, device):
+    summary_writer = SummaryWriter(os.path.join(save_path, "tb"))
 
     viz_samples = torch.stack(
-        [dataset[i][0] for i in np.random.choice(len(dataset), size=1024)]
+        [dataloader.dataset[i][0] for i in np.random.choice(len(dataloader), size=1024)]
     )
     epdf_interp = interpolate_samples(viz_samples, scale_factors, padding_mode="zeros")
     epdf_masks = interpolate_samples(
@@ -116,83 +163,113 @@ def main():
         len(np.unique(epdf_interp[-1], axis=0)) < 64
     ), "Lowest resolution is not densely sampled"
 
-    global_step = 0
-    for epoch in it.count(1):
-        torch.save(
-            {"model": model.state_dict(), "optim": optim.state_dict()},
-            first_unique_filename(
-                os.path.join(options.save_path, "checkpoints", f"epoch-{epoch:04d}.pkl")
-            ),
-        )
+    losses = []
+    cur_epoch = 0
+    progress = tqdm(dataloader)
+    for global_step, (epoch, loss) in enumerate(
+        training_loop(progress, model, optim, scale_factors, device)
+    ):
+        if epoch > cur_epoch:
+            if losses != []:
+                logging.info(
+                    "  //  ".join(
+                        [
+                            f"EPOCH {cur_epoch:d}",
+                            f"LOSS = {np.mean(losses):.3e}",
+                        ]
+                    )
+                )
+                losses = []
 
-        ## Visualization
-        model.eval()
-        rng_state = torch.get_rng_state()
-        np.random.seed(0)
-        torch.manual_seed(0)
+            cur_epoch = epoch
 
-        sample_idxs = np.random.choice(epdf_interp.size(1), size=8)
+            torch.save(
+                {"model": model.state_dict(), "optim": optim.state_dict()},
+                first_unique_filename(
+                    os.path.join(save_path, "checkpoints", f"epoch-{epoch:04d}.pkl")
+                ),
+            )
 
-        # Sampling
-        samples = [epdf_interp[-1, sample_idxs]]
-        for mask in epdf_masks[:-1, sample_idxs].flip(0):
-            with torch.no_grad():
-                x = model(samples[-1].to(device)).sample().cpu()
-            x = x.clamp(-1.0, 1.0)
-            x[~mask] = 0.0
-            samples.append(x)
-        samples = torch.stack(samples)
-        samples = (samples + 1.0) / 2
-        summary_writer.add_image(
-            "samples/generative",
-            make_grid(
-                samples.transpose(0, 1).reshape(-1, *samples.shape[2:]),
-                nrow=samples.shape[0],
-            ),
-            global_step=global_step,
-        )
+            ## Visualization
+            model.eval()
+            rng_state = torch.get_rng_state()
+            np.random.seed(0)
+            torch.manual_seed(0)
 
-        # Super resolution
-        samples = [
-            torch.nn.functional.pad(
-                viz_samples[sample_idxs],
+            sample_idxs = np.random.choice(epdf_interp.size(1), size=8)
+
+            # Sampling
+            samples = [epdf_interp[-1, sample_idxs]]
+            for mask in epdf_masks[:-1, sample_idxs].flip(0):
+                with torch.no_grad():
+                    x = model(samples[-1].to(device)).sample().cpu()
+                x = x.clamp(-1.0, 1.0)
+                x[~mask] = 0.0
+                samples.append(x)
+            samples = torch.stack(samples)
+            samples = (samples + 1.0) / 2
+            summary_writer.add_image(
+                "samples/generative",
+                make_grid(
+                    samples.transpose(0, 1).reshape(-1, *samples.shape[2:]),
+                    nrow=samples.shape[0],
+                ),
+                global_step=global_step,
+            )
+
+            # Super resolution
+            samples = [
+                torch.nn.functional.pad(
+                    viz_samples[sample_idxs],
+                    [
+                        (x + 1) // 2
+                        for x in viz_samples.shape[-2:][::-1]
+                        for _ in range(2)
+                    ],
+                    mode="constant",
+                    value=0.0,
+                )
+            ]
+            mask = torch.nn.functional.pad(
+                torch.ones_like(viz_samples[sample_idxs]),
                 [(x + 1) // 2 for x in viz_samples.shape[-2:][::-1] for _ in range(2)],
                 mode="constant",
                 value=0.0,
             )
-        ]
-        mask = torch.nn.functional.pad(
-            torch.ones_like(viz_samples[sample_idxs]),
-            [(x + 1) // 2 for x in viz_samples.shape[-2:][::-1] for _ in range(2)],
-            mode="constant",
-            value=0.0,
-        )
-        cur_scale_factor = 1.0
-        while cur_scale_factor < 2.0:
-            cur_scale_factor *= incremental_scale
-            mask = interpolate(mask, scale_factor=incremental_scale)
-            with torch.no_grad():
-                x = model(samples[-1].to(device)).sample().cpu()
-            x = x.clamp(-1.0, 1.0)
-            x[~mask.bool()] = 0.0
-            samples.append(x)
-        samples = torch.stack(samples)
-        samples = (samples + 1.0) / 2
-        summary_writer.add_image(
-            "samples/super-resolution",
-            make_grid(
-                samples.transpose(0, 1).reshape(-1, *samples.shape[2:]),
-                nrow=samples.shape[0],
-            ),
-            global_step=global_step,
-        )
+            cur_scale_factor = 1.0
+            while cur_scale_factor < 2.0:
+                incremental_scale = scale_factors[0] / scale_factors[1]
+                cur_scale_factor *= incremental_scale
+                mask = interpolate(mask, scale_factor=incremental_scale)
+                with torch.no_grad():
+                    x = model(samples[-1].to(device)).sample().cpu()
+                x = x.clamp(-1.0, 1.0)
+                x[~mask.bool()] = 0.0
+                samples.append(x)
+            samples = torch.stack(samples)
+            samples = (samples + 1.0) / 2
+            summary_writer.add_image(
+                "samples/super-resolution",
+                make_grid(
+                    samples.transpose(0, 1).reshape(-1, *samples.shape[2:]),
+                    nrow=samples.shape[0],
+                ),
+                global_step=global_step,
+            )
 
-        ## Trainig loop
-        model.train()
-        torch.set_rng_state(rng_state)
-        losses = []
-        progress = tqdm(dataloader)
-        for x, _ in progress:
+            torch.set_rng_state(rng_state)
+
+        torch.distributed.all_reduce(loss)
+        losses.append(loss.item())
+        progress.set_description(f"LOSS = {np.mean(losses):.2e}")
+        summary_writer.add_scalar("loss", loss, global_step=global_step)
+
+
+def training_loop(dataloader, model, optim, scale_factors, device):
+    for epoch in it.count(1):
+        for x, _ in dataloader:
+            model.train()
+
             x = x.to(device=device)
             data_masks = torch.ones_like(x)
 
@@ -200,33 +277,16 @@ def main():
             data_masks = interpolate_samples(
                 data_masks, scale_factors[:-1], padding_mode="zeros"
             )
-            if options.pixel_weights:
-                pixel_weights = data_masks / data_masks.sum((2, 3, 4), keepdim=True)
-            else:
-                pixel_weights = data_masks
 
             y = model(x[1:].reshape(-1, *x.shape[2:]))
-            lp = y.log_prob(x[:-1].reshape(-1, *x.shape[2:])).reshape_as(pixel_weights)
-            loss = -(lp * pixel_weights).sum((1, 2, 3, 4)).mean(0)
+            lp = y.log_prob(x[:-1].reshape(-1, *x.shape[2:])).reshape_as(data_masks)
+            loss = -(lp * data_masks).sum((1, 2, 3, 4)).mean(0)
 
             optim.zero_grad()
             loss.backward()
             optim.step()
 
-            losses.append(loss.item())
-            progress.set_description(f"LOSS = {np.mean(losses):.2e}")
-            summary_writer.add_scalar("loss", loss, global_step=global_step)
-
-            global_step += 1
-
-        logging.info(
-            "  //  ".join(
-                [
-                    f"EPOCH {epoch:d}",
-                    f"LOSS = {np.mean(losses):.3e}",
-                ]
-            )
-        )
+            yield epoch, loss.detach()
 
 
 if __name__ == "__main__":
