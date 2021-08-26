fix numerical issue

resolution_diffusion/__main__.py
@@ -188,7 +188,7 @@ def run(rank, options):
         data_masks = interpolate_samples(
             data_masks, scale_factors[:-1], padding_mode="zeros"
         )
-        data_masks = data_masks.floor()
+        data_masks = (data_masks > 0.99).float()
 
         y = model(x[1:].reshape(-1, *x.shape[2:]))
         lp = y.log_prob(x[:-1].reshape(-1, *x.shape[2:])).reshape_as(data_masks)
@@ -267,7 +267,7 @@ def run(rank, options):
             incremental_scale = scale_factors[0] / scale_factors[1]
             cur_scale_factor *= incremental_scale
             mask = interpolate(mask, scale_factor=incremental_scale)
-            mask = mask.floor()
+            mask = (mask > 0.99).float()
             with torch.no_grad():
                 x = model(samples[-1].to(device)).sample().cpu()
             x = x.clamp(-1.0, 1.0)